9ffe883306ea2c2ed8c38b100f6b8105c1906623
Gregory Collett <gregcollett@mixcloud.com>
2023-02-28 11:11:39 +0000

    Update isHotReloading.js

--- src/util/isHotReloading.js
+++ src/util/isHotReloading.js
@@ -1,12 +1,6 @@
 // @flow
 const isHotReloading = (): boolean => {
-  const castModule: any = typeof module !== 'undefined' && module
-  return !!(
-    castModule &&
-    castModule.hot &&
-    typeof castModule.hot.status === 'function' &&
-    castModule.hot.status() === 'apply'
-  )
+  return false;
 }
 
 export default isHotReloading
